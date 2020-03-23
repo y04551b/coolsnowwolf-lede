04934665c704728e7cf0de920c8451d1ecb4d97e
tcsr200722 <350524166@qq.com>
2020-03-23 09:16:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,8 +10,10 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 #add luci-app-koolproxyR
 git clone https://github.com/tcsr200722/luci-app-koolproxyR.git package/luci-app-koolproxyR
 #add luci-app-diskman
-mkdir -p package/luci-app-diskman && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/luci-app-diskman/Makefile
-mkdir -p package/parted && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+mkdir -p package/luci-app-diskman && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/luci-app-diskman/Makefile
+mkdir -p package/parted && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
 #add luci-app-filebrowser
 git clone https://github.com/project-openwrt/FileBrowser.git package/luci-app-filebrowser
 #add OpenAppFilter
